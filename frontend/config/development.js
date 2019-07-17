d4add4ebf29b81320a81254d1c3f87f44965d95e
kozo yamagata <tune002@gmail.com>
2019-07-17 23:34:55 +0900

    Build environment for frontend by webpack

---
+++ frontend/config/development.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './frontend/src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/assets/'
+  },
+  devServer: {
+    contentBase: './frontend/dist'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './frontend/public/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
---
+++ frontend/src/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const appNode = document.getElementById('app')
+  console.log(appNode)
+  appNode.innerHTML = 'Hello World'
+})

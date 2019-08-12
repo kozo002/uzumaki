d7408c4621092d6d1201f1bb062657c8dff16863
kozo yamagata <tune002@gmail.com>
2019-08-12 19:53:26 +0900

    Delete unnecessary codes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,34 +1,11 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
-
-const schema = gql`
-  query {
-    users {
-      id
-    }
-  }
-`
-
-interface User {
-  id: number
-}
 
 export default function Home () {
-  const { loading, error, data } = useQuery(schema, { variables: {} })
-
-  if (loading) { return 'loading' }
-  if (error) {
-    console.error(error)
-    return 'error'
-  }
-
   return (
     <div>
       Home
       <Link to="/logout">logout</Link>
-      {data.users.map((user: User, i: number) => <div key={i}>user {user.id}</div>)}
     </div>
   )
 }

1a16ef065fdb20eed162616ebd66da4f2f8d76ee
kozo yamagata <tune002@gmail.com>
2019-08-14 23:40:01 +0900

    Protect graphql by user authentication

--- backend/app/graphql/index.js
+++ backend/app/graphql/index.js
@@ -1,10 +1,11 @@
 const glob = require('glob')
 const path = require('path')
 const fs = require('fs')
-const { ApolloServer, gql } = require('apollo-server-express')
+const { ApolloServer, gql, AuthenticationError } = require('apollo-server-express')
 const { createContext } = require('dataloader-sequelize')
 
 const db = require('../models')
+const JWT = require('../utils/jwt')
 
 const schemaPaths = glob.sync(path.resolve(__dirname, 'schemas/*.graphql'))
 const schemas = schemaPaths.map(it => fs.readFileSync(it, { encoding: 'utf-8' })).join("\n")
@@ -16,12 +17,30 @@ const resolvers = resolverPaths.reduce((acc, it) => {
   return { ...acc, [name]: require(it) }
 }, {})
 
+const env = process.env.NODE_ENV || 'development'
+
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    db,
-    eok: createContext(db.sequelize),
+  context: async ({ req, res }) => {
+    const token = (req.headers.authorization || '').split(' ')[1]
+    let decoded, user
+
+    try {
+      decoded = JWT.decode(token)
+      user = await db.User.findOne({ where: { id: decoded.user_id } })
+    } catch (err) {
+      console.error(err)
+    }
+    if (!user) {
+      throw new AuthenticationError()
+    }
+
+    return {
+      db,
+      eok: createContext(db.sequelize),
+      user,
+    }
   }
 })
 

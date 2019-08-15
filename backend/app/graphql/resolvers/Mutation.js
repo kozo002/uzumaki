970b147fdc651af3b8d4c9e976a3a79e5f7452ad
kozo yamagata <tune002@gmail.com>
2019-08-15 17:44:26 +0900

    Implement createProject mutation

--- backend/app/graphql/resolvers/Mutation.js
+++ backend/app/graphql/resolvers/Mutation.js
@@ -18,5 +18,27 @@ module.exports = {
       throw err
     }
     return organization
+  },
+
+  createProject: async (parent, args, context, info) => {
+    const { organizationId, name, description } = args
+    const { user, db } = context
+    const organization = await db.Organization.findOneBelonggingToUser({
+      id: organizationId,
+      userId: user.id,
+    })
+    if (!organization) {
+      throw new ForbiddenError('You do not belong to the specified organization')
+    }
+    let project
+    try {
+      project = await organization.createProject({ name, description })
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        throw new UserInputError(err.message)
+      }
+      throw err
+    }
+    return project
   }
 }
--- backend/test/graphql/resolvers/Mutation.test.js
+++ backend/test/graphql/resolvers/Mutation.test.js
@@ -88,4 +88,82 @@ describe('Mutation', () => {
       })
     })
   })
+
+  describe('createProject', () => {
+    describe('when target organization belongs to the user', () => {
+      describe('when the args are valid', () => {
+        it('creates a new project', async () => {
+          const db = require('../../../app/models')
+          const user = await db.User.create({
+            name: 'example',
+            email: 'example@example.com',
+            githubId: 1,
+          })
+          const organization = await user.createOrganization({ name: 'example' })
+          const dummyArgs = {
+            organizationId: organization.id,
+            name: 'example name',
+            description: 'example description',
+          }
+          const dummyContext = { user, db }
+          expect(await db.Project.count()).toEqual(0)
+          const project = await Mutation.createProject(null, dummyArgs, dummyContext)
+          expect(await db.Project.count()).toEqual(1)
+          expect(project.name).toEqual('example name')
+          expect(project.description).toEqual('example description')
+        })
+      })
+
+      describe('when the args are not valid', () => {
+        it('throws UserInputError', async () => {
+          const db = require('../../../app/models')
+          const user = await db.User.create({
+            name: 'example',
+            email: 'example@example.com',
+            githubId: 1,
+          })
+          const organization = await user.createOrganization({ name: 'example' })
+          const dummyArgs = {
+            organizationId: organization.id,
+            name: '',
+            description: 'example description',
+          }
+          const dummyContext = { user, db }
+          expect(await db.Project.count()).toEqual(0)
+          try {
+            await Mutation.createProject(null, dummyArgs, dummyContext)
+          } catch (err) {
+            expect(err).toBeInstanceOf(UserInputError)
+          }
+          expect(await db.Project.count()).toEqual(0)
+        })
+      })
+    })
+
+    describe('when target organization does not belong to the user', () => {
+      it('throws ForbiddenError', async () => {
+        const db = require('../../../app/models')
+        const user = await db.User.create({
+          name: 'example',
+          email: 'example@example.com',
+          githubId: 1,
+        })
+        const user2 = await db.User.create({
+          name: 'example 2',
+          email: 'example2@example.com',
+          githubId: 2,
+        })
+        const organization = await user2.createOrganization({ name: 'example' })
+        const dummyArgs = { organizationId: organization.id }
+        const dummyContext = { user, db }
+        expect(await db.Project.count()).toEqual(0)
+        try {
+          await Mutation.createProject(null, dummyArgs, dummyContext)
+        } catch (err) {
+          expect(err).toBeInstanceOf(ForbiddenError)
+        }
+        expect(await db.Project.count()).toEqual(0)
+      })
+    })
+  })
 })

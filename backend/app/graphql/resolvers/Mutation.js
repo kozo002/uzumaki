8d819dab95f9baa3ca8e07748920e9f24ebb38c8
kozo yamagata <tune002@gmail.com>
2019-08-15 17:58:36 +0900

    Implement updateProject mutation

--- backend/app/graphql/resolvers/Mutation.js
+++ backend/app/graphql/resolvers/Mutation.js
@@ -40,5 +40,23 @@ module.exports = {
       throw err
     }
     return project
-  }
+  },
+
+  updateProject: async (parent, args, context, info) => {
+    const { id, name, description } = args
+    const { user, db } = context
+    const project = await db.Project.findOneBelonggingToUser({ id, userId: user.id })
+    if (!project) {
+      throw new ForbiddenError('You do not belong to the specified project')
+    }
+    try {
+      await project.update({ name, description })
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        throw new UserInputError(err.message)
+      }
+      throw err
+    }
+    return project
+  },
 }
--- backend/test/graphql/resolvers/Mutation.test.js
+++ backend/test/graphql/resolvers/Mutation.test.js
@@ -166,4 +166,85 @@ describe('Mutation', () => {
       })
     })
   })
+
+  describe('updateProject', () => {
+    describe('when target project belongs to the user', () => {
+      describe('when the args are valid', () => {
+        it('updates the project', async () => {
+          const db = require('../../../app/models')
+          const user = await db.User.create({
+            name: 'example',
+            email: 'example@example.com',
+            githubId: 1,
+          })
+          const organization = await user.createOrganization({ name: 'example' })
+          const project = await organization.createProject({ name: 'example' })
+          const dummyArgs = {
+            id: project.id,
+            name: 'updated name',
+            description: 'updated description',
+          }
+          const dummyContext = { user, db }
+          expect(await db.Project.count()).toEqual(1)
+          const updatedProject = await Mutation.updateProject(null, dummyArgs, dummyContext)
+          expect(updatedProject.name).toEqual(dummyArgs.name)
+          expect(updatedProject.description).toEqual(dummyArgs.description)
+          expect(await db.Project.count()).toEqual(1)
+        })
+      })
+
+      describe('when the args are not valid', () => {
+        it('throws UserInputError', async () => {
+          const db = require('../../../app/models')
+          const user = await db.User.create({
+            name: 'example',
+            email: 'example@example.com',
+            githubId: 1,
+          })
+          const organization = await user.createOrganization({ name: 'example' })
+          const project = await organization.createProject({ name: 'example' })
+          const dummyArgs = {
+            id: project.id,
+            name: '',
+            description: 'updated description',
+          }
+          const dummyContext = { user, db }
+          try {
+            await Mutation.updateProject(null, dummyArgs, dummyContext)
+          } catch (err) {
+            expect(err).toBeInstanceOf(UserInputError)
+          }
+        })
+      })
+    })
+
+    describe('when target project does not belong to the user', () => {
+      it('throws ForbiddenError', async () => {
+        const db = require('../../../app/models')
+        const user = await db.User.create({
+          name: 'example',
+          email: 'example@example.com',
+          githubId: 1,
+        })
+        const user2 = await db.User.create({
+          name: 'example 2',
+          email: 'example2@example.com',
+          githubId: 2,
+        })
+        const organization = await user2.createOrganization({ name: 'example' })
+        const project = await organization.createProject({ name: 'example' })
+        const dummyArgs = {
+          id: project.id,
+          name: 'updated name',
+          description: 'updated description',
+        }
+        const dummyContext = { user, db }
+        try {
+          await Mutation.updateProject(null, dummyArgs, dummyContext)
+        } catch (err) {
+          expect(err).toBeInstanceOf(ForbiddenError)
+        }
+      })
+    })
+  })
 })

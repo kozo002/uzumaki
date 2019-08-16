468c184aa872b9775e13e56156cff50437b1f09a
kozo yamagata <tune002@gmail.com>
2019-08-16 17:13:08 +0900

    Fix Auth container

--- frontend/src/containers/Auth.tsx
+++ frontend/src/containers/Auth.tsx
@@ -1,40 +1,15 @@
 import * as React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 import Session from '../models/Session'
 
 type Props = {
   children: React.ReactNode,
 }
-type State = {
-  isAuthenticated: boolean,
-}
 
-export default class Auth extends React.Component<Props, State> {
-  constructor (props: Props) {
-    super(props)
-    this.state = {
-      isAuthenticated: false,
-    } as State
+export default function Auth (props: Props) {
+  if (!Session.isLoggedIn) {
+    return <Redirect to={'/login'} />
   }
-
-  componentWillMount () {
-    this.checkAccessToken()
-  }
-
-  componentWillUpdate () {
-    this.checkAccessToken()
-  }
-
-  checkAccessToken () {
-    this.setState({ isAuthenticated: Session.isLoggedIn });
-  }
-
-  render () {
-    if (this.state.isAuthenticated) {
-      return <Route children={this.props.children} />
-    } else {
-      return <Redirect to="/login" />
-    }
-  }
+  return <>{props.children}</>
 }

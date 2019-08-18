168d0c4161d5d94112fd4cc832b39f608d6cb439
kozo yamagata <tune002@gmail.com>
2019-08-18 16:12:43 +0900

    Add Authorization header to apollo request

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,9 +1,29 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+
+import Session from '../models/Session'
+
+const httpLink = createHttpLink({
+  uri: `${process.env.API_ORIGIN}/graphql`
+})
+
+const authLink = setContext(async (operation: any, { headers }: any): Promise<any> => {
+  const token = Session.token
+  if (!token) {
+    return { headers }
+  }
+  return {
+    headers: {
+      ...headers,
+      'Authorization': `token ${token}`,
+    }
+  }
+})
 
 const client = new ApolloClient({
-  link: createHttpLink({ uri: `${process.env.API_ORIGIN}/graphql` }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 

674ede40b525f09b0b1e2e399582adcd378d34ee
kozo yamagata <tune002@gmail.com>
2019-08-24 13:26:39 +0900

    Add the handler for authentication error to apollo client

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,7 +1,9 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
+import { ErrorLink } from 'apollo-link-error'
 
 import Session from '../models/Session'
 
@@ -22,8 +24,19 @@ const authLink = setContext(async (operation: any, { headers }: any): Promise<an
   }
 })
 
+const logoutLink = new ErrorLink((err) => {
+  const unauthenticated = err.graphQLErrors.find(err => err.extensions.code === 'UNAUTHENTICATED')
+  if (!unauthenticated) { return }
+  Session.logout()
+  window.location.reload()
+})
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([
+    authLink,
+    logoutLink,
+    httpLink,
+  ]),
   cache: new InMemoryCache(),
 })
 

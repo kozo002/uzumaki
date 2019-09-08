065c1601b5e32022bbfbedf2b7590b3def87ab15
kozo yamagata <tune002@gmail.com>
2019-09-08 15:03:16 +0900

    Place story details table on Detail component

--- frontend/src/components/Story/Detail.tsx
+++ frontend/src/components/Story/Detail.tsx
@@ -2,7 +2,10 @@ import * as React from 'react'
 import Markdown from 'react-markdown'
 
 import Story from '@/models/Story'
+import StoryState from '@/models/StoryState'
 import EditableHeading from '@/components/EditableHeading'
+import StateButton from '@/components/Story/StateButton'
+import TypeIcon from '@/components/Story/TypeIcon'
 
 type Props = {
   story: Story,
@@ -19,7 +22,58 @@ export default function Detail (props: Props) {
         tagLevel="h1"
         onSave={(title: string) => onUpdate(story, { title })}
       />
-      <Markdown source={story.description} />
+
+      <div className="d-flex">
+        <table className="table table-sm mb-3 mr-2" style={{ width: '50%' }}>
+          <tbody>
+            <tr>
+              <th>Story Type</th>
+              <td>
+                <TypeIcon
+                  type={story.type}
+                />
+                {story.type}
+              </td>
+            </tr>
+            <tr>
+              <th>Points</th>
+              <td>
+                {story.points === null ? (
+                  <span className="text-secondary">No Points</span>
+                ) : story.points}
+              </td>
+            </tr>
+            <tr>
+              <th>Requester</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>Owner</th>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        
+        <table className="table table-sm ml-2" style={{ width: '50%' }}>
+          <tbody>
+            <tr>
+              <th>State</th>
+              <td>
+                <StateButton
+                  state={story.state}
+                  onChange={(state: StoryState) => onUpdate(story, { state })}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="card">
+        <div className="card-body">
+          <Markdown source={story.description} />
+        </div>
+      </div>
     </article>
   )
 }
